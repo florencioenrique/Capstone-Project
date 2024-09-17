fe9f1a34b32167c4d3d865ac3b1d02f7cab0f543
EnriqueCute <florencioenrique69@gmail.com>
2024-09-17 22:18:51 +0800

    Add files via upload

---
+++ Landing Page/javascript/function.js
@@ -0,0 +1,11 @@
+const getStartedButton = document.getElementById('gs');
+const formContainer = document.getElementById('form-container');
+
+getStartedButton.addEventListener('click', function() {
+    formContainer.classList.add('visible');
+        document.getElementById("gs").i;
+});
+
+function get_started(){
+    document.getElementById('section1').style.display = 'none';
+}
